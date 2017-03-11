df2bfdc4b7b960ec5134e098878bfbe23d34892c
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-03-11 19:26:46 +0100

    Added demo files

---
+++ first/src/Solver.h
@@ -0,0 +1,29 @@
+
+
+class CSolver
+{
+public:
+    static void Solve(ACenter x)
+    {}
+
+    static void Solve(ARedundancy x)
+    {}
+
+    CSolver(void)
+    {}
+
+    ~CSolver(void)
+    {}
+
+    void Start(int thrCnt)
+    {}
+
+    void Stop(void)
+    {}
+
+    void AddCustomer(ACustomer c)
+    {}
+
+private:
+    // todo
+};
