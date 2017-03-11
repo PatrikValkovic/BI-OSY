daa9737bb56ecd469916319088b5e287931b3cea
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-03-11 19:44:12 +0100

    Added counting of vertex

--- first/src/Solver.h
+++ first/src/Solver.h
@@ -6,7 +6,15 @@ class CSolver
 {
 public:
     static void Solve(shared_ptr<CCenter> x)
-    {    }
+    {
+        set<string> vertex;
+        for(CLink link : x->m_Links)
+        {
+            vertex.insert(link.m_From);
+            vertex.insert(link.m_To);
+        }
+        cout << "Count of vertex: " << vertex.size() << endl;
+    }
 
     static void Solve(shared_ptr<CRedundancy> x)
     {}
